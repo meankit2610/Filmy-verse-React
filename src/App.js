f403d73ee7a8cb7896c8a61b07b5030f2a647625
meankit2610 <ankitsingh5282770@gmail.com>
2023-02-27 18:12:39 +0530

    Add-Movie-section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Cards from "./components/Cards";
 import Header from "./components/Header";
-
+import {Route, Routes} from "react-router-dom"
+import AddMovie from "./components/AddMovie";
 function App() {
   return (
     <div className="App"> 
-      <Header/>
-      <Cards/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Cards/>} />
+        <Route path="/addMovie" element={<AddMovie/>} />
+      </Routes>
+      
     </div>
   );
 }
---
+++ src/components/AddMovie.jsx
@@ -0,0 +1,77 @@
+import React,{useState} from 'react'
+
+const AddMovie = () => {
+    const [form, setform] = useState({
+        "title": "",
+        "year": "",
+        "description":""
+    })
+  return (
+    <div>
+      <section class="text-gray-400 bg-gray-900 body-font relative">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-col text-center w-full mb-12">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
+              Add Movie
+            </h1>
+          </div>
+          <div class="lg:w-1/2 md:w-2/3 mx-auto">
+            <div class="flex flex-wrap -m-2">
+              <div class="p-2 w-1/2">
+                <div class="relative">
+                  <label for="name" class="leading-7 text-sm text-white">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={form.title}
+                    onChange={(e)=>setform({...form,title:e.target.value})}
+                    class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+              <div class="p-2 w-1/2">
+                <div class="relative">
+                  <label for="email" class="leading-7 text-sm text-white">
+                    Year
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={form.year}
+                    onChange={(e)=>setform({...form, title:e.target.value})}
+                    class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+              <div class="p-2 w-full">
+                <div class="relative">
+                  <label for="message" class="leading-7 text-sm text-white">
+                    Description
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    value={form.description}
+                    onChange={(e)=>setform({...form,title: e.target.value})}
+                    class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                  ></textarea>
+                </div>
+              </div>
+              <div class="p-2 w-full">
+                <button class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">
+                  Button
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default AddMovie
